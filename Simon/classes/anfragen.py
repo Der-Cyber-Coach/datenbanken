c066e2b06cc84484b64bd901c9815070e40f3530
Simon Löschke <simon-loeschke@posteo.de>
2023-10-03 22:39:23 +0200

    erste abfrage

--- Simon/classes/anfragen.py
+++ Simon/classes/anfragen.py
@@ -13,20 +13,6 @@ class XmlRequestHandler:
 
             # Überprüfen, ob die Anfrage erfolgreich war (Status-Code 200)
             if response.status_code == 200:
-                # XML-Datei erstellen und den Wurzelknoten festlegen
-                root = ET.Element('response')
-
-                # Hinzufügen des Response-Texts als XML-Element
-                response_element = ET.SubElement(root, 'data')
-                response_element.text = response.text
-
-                # Erstellen Sie ein ElementTree-Objekt aus dem Wurzelknoten
-                tree = ET.ElementTree(root)
-
-                # XML-Datei speichern
-                with open('response.xml', 'wb') as xml_file:
-                    tree.write(xml_file, encoding='utf-8')
-
                 # Die XML-Response schöner formatieren
                 xml_data = minidom.parseString(response.text)
                 pretty_xml = xml_data.toprettyxml()
@@ -34,7 +20,6 @@ class XmlRequestHandler:
                 with open('pretty_response.xml', 'w', encoding='utf-8') as pretty_xml_file:
                     pretty_xml_file.write(pretty_xml)
 
-                print('Response wurde in response.xml gespeichert.')
                 print('Schön formatierte Response wurde in pretty_response.xml gespeichert.')
             else:
                 print('Fehler beim Senden des Requests. Status-Code:', response.status_code)
@@ -43,6 +28,8 @@ class XmlRequestHandler:
 
 # Beispielverwendung der XmlRequestHandler-Klasse
 if __name__ == "__main__":
-    url = 'https://www.fivb.org/vis2009/XmlRequest.asmx?Request=%3CRequests%3E%3CRequest%20Type=%27GetBeachMatch%27%20No=%2715592%27%20Fields=%27NoInTournament%20LocalDate%20LocalTime%20TeamAType%20TeamAName%20TeamBType%20TeamBName%20Court%20MatchPointsA%20MatchPointsB%20PointsTeamASet1%20PointsTeamBSet1%20PointsTeamASet2%20PointsTeamBSet2%20PointsTeamASet3%20PointsTeamBSet3%20DurationSet1%20DurationSet2%20DurationSet3%27/%3E%3C/Requests%3E'
+    # URL mit dem angegebenen Request und dem richtigen Encoding
+    url = f"https://www.fivb.org/vis2009/XmlRequest.asmx?Request=<Request Type='GetBeachMatchList' Fields='NoInTournament LocalDate LocalTime TeamAName TeamBName Court MatchPointsA MatchPointsB PointsTeamASet1 PointsTeamBSet1 PointsTeamASet2 PointsTeamBSet2 PointsTeamASet3 PointsTeamBSet3 DurationSet1 DurationSet2 DurationSet3'><Filter /> <!-- optional: contains the filter to use --> </Request>"
+
     request_handler = XmlRequestHandler(url)
     request_handler.send_request()
